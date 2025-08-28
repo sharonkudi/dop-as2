8720fa840c12cfab7e96bd780bc98e49f232e7fa
sharon <23FTT2881@student.pb.edu.bn>
2025-08-28 21:22:32 +0800

    Update app-logo.blade.php

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <img src="{{ asset('images/LogoPB.png') }}" alt="logo" class="w-full h-full object-contain" />
+    <img src="{{ asset('image/LogoPB.png') }}" alt="logo" class="w-full h-full object-contain" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
