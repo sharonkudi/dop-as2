27c49a167a76030fc81316bfecd88b28442b09fa
sharon <23FTT2881@student.pb.edu.bn>
2025-08-28 21:36:10 +0800

    Update app-logo.blade.php

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,6 +3,6 @@
     <img src="{{ asset('image/pblogo.png') }}" alt="logo" class="w-full h-full object-contain" />
 {{-- </div> --}}
 {{-- <div class="ms-1 grid flex-1 text-start text-sm"> --}}
-    <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span> --}}
+  {{--  <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span> --}}
 {{-- </div> --}}
 
